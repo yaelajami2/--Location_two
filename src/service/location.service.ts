4c58ae71a0c9034ca88f3d3831efdb8495af0e9b
yaelajami2 <y0556722091@gmail.com>
2024-09-29 21:16:41 +0300

    id

--- src/service/location.service.ts
+++ src/service/location.service.ts
@@ -9,7 +9,7 @@ import { Location } from '../Models/location';
   providedIn: 'root'
 })
 export class LocationService {
-  apiUrl="https://location-mongo.onrender.com/api/location";
+  apiUrl="https://localhost:44316/api/location";
   list_location=new BehaviorSubject<Location[]>([]);
   data$: Observable<Location[]> = this.list_location.asObservable(); 
   constructor(private http:HttpClient) {   this.get_location();}
@@ -34,7 +34,7 @@ export class LocationService {
      
       
 
-  return this.http.post<any>(this.apiUrl+"/insertById", { id: 0 ,name:Name} ,{headers}
+  return this.http.post<any>(this.apiUrl+"/insertById", { id: "0" ,name:Name} ,{headers}
   
    ).pipe( 
     map(() => {
